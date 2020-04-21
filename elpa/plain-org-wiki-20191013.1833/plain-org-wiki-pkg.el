dc34008b3ee87bff11af3ed9b757466feb8076ec
Gus Cantieni <gus.cantieni@gmail.com>
2020-04-21 09:01:53 -0400

    Migrate away from package.el and delete elpa

--- elpa/plain-org-wiki-20191013.1833/plain-org-wiki-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plain-org-wiki" "20191013.1833" "Simple jump-to-org-files in a directory package" '((emacs "24.3") (ivy "0.12.0")) :commit "887717c184fb22dd219c78851303a8e5b917f877" :keywords '("convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/plain-org-wiki")
